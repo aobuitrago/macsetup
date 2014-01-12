a083c8be14a2a03cb4339b61634a88a9db20f219
ossian <ossian@canarytek.com>
2014-01-12 19:46:24 +0000

    some lines commented out

--- setup.sh
+++ setup.sh
@@ -14,13 +14,13 @@
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
-x#source $HOME/.nvm/nvm.sh
-x#nvm install v0.10.12
-x#nvm use v0.10.12
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.12
+nvm use v0.10.12
 
 # Install jshint to allow checking of JS code within emacs and vim
 # http://jshint.com/
-x#npm install -g jshint
+npm install -g jshint
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
